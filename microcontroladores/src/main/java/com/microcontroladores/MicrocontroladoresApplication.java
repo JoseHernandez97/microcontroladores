fe5ce5ef817e1d87a8e35dd9454b582ff2c5e764
JoseHernandez97 <josemanuelhl1997@gmail.com>
2023-01-14 15:43:16 -0500

    first commit

---
+++ microcontroladores/src/main/java/com/microcontroladores/MicrocontroladoresApplication.java
@@ -0,0 +1,13 @@
+package com.microcontroladores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrocontroladoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrocontroladoresApplication.class, args);
+	}
+
+}
